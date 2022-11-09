9013d83756e221397c5268754e4106b3477b3bd2
Setya Adi Kurnia <engineering.ins@weq-tech.com>
2022-11-09 15:31:57 +0700

    Changes due to changes on core library

--- Adita.PlexNet.Wpf.Dialogs/Extensions/DependencyInjection/DialogServiceCollectionExtensions.cs
+++ Adita.PlexNet.Wpf.Dialogs/Extensions/DependencyInjection/DialogServiceCollectionExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Adita.PlexNet.Core.Dialogs;
 using Adita.PlexNet.Wpf.Dialogs;
+using Adita.PlexNet.Wpf.Dialogs.Services;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Windows;
 
 namespace Adita.PlexNet.Wpf.Extensions.DependencyInjection
 {
@@ -10,21 +12,34 @@ namespace Adita.PlexNet.Wpf.Extensions.DependencyInjection
     /// </summary>
     public static class DialogServiceCollectionExtensions
     {
+        #region Public methods
         /// <summary>
         /// Adds dialog environment to specified <paramref name="services"/> using specified <paramref name="builderAction"/> to register dialogs.
         /// </summary>
         /// <param name="services">An <see cref="IServiceCollection"/> to add the dialog environment to.</param>
         /// <param name="builderAction">An action to register dialogs.</param>
         /// <returns>An <see cref="IServiceCollection"/> to chain the operations.</returns>
-        public static IServiceCollection AddDialogs(this IServiceCollection services, Action<IDialogBuilder> builderAction)
+        public static IServiceCollection AddDialogs(this IServiceCollection services, Action<DialogBuilder> builderAction)
         {
-            services.AddScoped<IDialogProvider, DialogProvider>()
-                    .AddScoped<IDialogService, DialogService>()
-                    .AddScoped<IDialogContainerFactory, DialogContainerFactory>();
+            DialogBuilder builder = new(services);
+            builder.AddDialogHostProvider<DialogHostProvider>()
+            .AddDialogViewProvider<DialogViewProvider>()
+            .AddMessageService()
+            .ConfigureDialogOptions(configure =>
+            {
+                configure.ContainerStandardType = typeof(DialogContainer);
+                configure.ContainerWithReturnType = typeof(DialogContainer<>);
+                configure.ContainerWithReturnAndParamType = typeof(DialogContainer<,>);
+                configure.ContainerOnlyParamType = typeof(ParamOnlyDialogContainer<>);
+                configure.HostType = typeof(Window);
+                configure.ViewType = typeof(DataTemplate);
+                configure.MessageViewType = typeof(MessageView);
+            });
 
-            builderAction.Invoke(new DialogBuilder(services));
+            builderAction.Invoke(builder);
 
-            return services;
+            return builder.Build().Services;
         }
+        #endregion Public methods
     }
 }
